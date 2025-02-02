f398ea482197c5e1c2277bbb7a6c3209a553b03e
donghwan <ydhwan0905@gmail.com>
2025-02-02 18:34:57 +0900

    스프링 시큐리티-초기로그인화면 비활성화 설정

---
+++ src/main/java/com/study/rest_board/config/SecurityConfig.java
@@ -0,0 +1,16 @@
+package com.study.rest_board.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+	@Bean
+	public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+		http.formLogin(AbstractHttpConfigurer::disable);
+		return http.build();
+	}
+}
