2319deab2426bdc9044bc3632788e90fd5c9e661
donghwan <ydhwan0905@gmail.com>
2025-02-09 10:11:00 +0900

    배포 환경설정

--- src/main/java/com/study/rest_board/RestBoardApplication.java
+++ src/main/java/com/study/rest_board/RestBoardApplication.java
@@ -2,8 +2,10 @@ package com.study.rest_board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
 public class RestBoardApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/study/rest_board/RestBoardApplicationTests.java
+++ src/test/java/com/study/rest_board/RestBoardApplicationTests.java
@@ -2,8 +2,10 @@ package com.study.rest_board;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootTest
+@EnableWebMvc
 class RestBoardApplicationTests {
 
 	@Test
