d1e54ac0b8be38f34b26f8a50a6087851625627e
donghwan <ydhwan0905@gmail.com>
2025-02-09 10:23:16 +0900

    배포 환경설정

--- src/test/java/com/study/rest_board/RestBoardApplicationTests.java
+++ src/test/java/com/study/rest_board/RestBoardApplicationTests.java
@@ -1,15 +1,15 @@
-package com.study.rest_board;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-@SpringBootTest
-@EnableWebMvc
-class RestBoardApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.study.rest_board;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//
+//@SpringBootTest
+//@EnableWebMvc
+//class RestBoardApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
